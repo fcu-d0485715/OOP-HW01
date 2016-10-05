d06eaaefab2f580b695179931e1c1a41a74ac65b
Qoo <Qoo@QQ>
2016-10-05 20:23:53 +0800

    homework01

---
+++ src/fcu/iecs/oop/homework01.java
@@ -0,0 +1,19 @@
+package fcu.iecs.oop;
+
+public class homework01 {
+
+	public static void main(String[] args) {
+		
+String lyrics = "Let it go! Let it go! Cannot hold it back anymore";
+		
+		lyrics = lyrics.replace("it", "\"her\"");
+		
+		lyrics = lyrics.replace("Cannot", "Can't");
+	     
+		System.out.println(lyrics);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
